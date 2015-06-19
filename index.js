dde9034901f772db40b75933ac86c7398646cb37
Tim Banks <thetimbanks@gmail.com>
2015-06-19 15:36:35 -0500

    Changed urban to post back as urban bot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var urban = require('urban');
 var bodyParser = require('body-parser');
+var request = require('request')
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -14,18 +15,35 @@ app.get('/', function(request, response) {
   response.send("Slack Slash home page");
 });
 
-app.post('/urban', function(request, response) {
-	var word = urban(request.body.text);
+app.post('/urban', function(req, res) {
+	var word = urban('inboob');
 
-	word.first(function(json) {
-		var payload = [
-			'Definitiion: ' + json.definition,
-			'Example: ' + json.example,
-			'Link: <' + json.permalink + '>'
-		];
+    word.first(function(json) {
+        var payload = [
+            'Definitiion: ' + json.definition,
+            'Example: ' + json.example,
+            'Link: <' + json.permalink + '>'
+        ];
 
-	    response.send(payload.join("\n"));
-	});
+        var channel = req.body.channel_name === 'directmessage' ?
+            req.body.channel_id : '#' + req.body.channel_name;
+
+        payload = {
+            channel: channel,
+            text: payload.join("\n")
+        };
+
+        request.post({
+            url: 'https://hooks.slack.com/services/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',
+            form: { payload: JSON.stringify(payload) }
+            }, function (err, resp, body) {
+            if (err) {
+              return res.status(500).send({ success: false, error: err.message });
+            }
+
+            res.end();
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {

92816baf725c512de066c40d85073d63773a5ab5
Tim Banks <thetimbanks@gmail.com>
2017-04-04 14:02:51 -0500

    Add /fmsversion endpoint to see fms normie release branch

--- index.js
+++ index.js
@@ -47,6 +47,48 @@ app.post('/urban', function(req, res) {
     });
 });
 
+app.post('/fmsversion', function(req, res) {
+    request.get({
+        url: 'https://jenkins.rnl.io/job/FMS/view/FMS%20Production/job/fms-production/api/json',
+        auth: {
+                user: process.env.JENKINS_USER,
+                pass: process.env.JENKINS_API_KEY
+            }
+        }, function (err, resp, body) {
+            var jsonBody = JSON.parse(body);
+            var latestSuccessfulBuild = jsonBody.lastSuccessfulBuild;
+            request.get({
+                url: latestSuccessfulBuild.url + 'api/json',
+                auth: {
+                        user: process.env.JENKINS_USER,
+                        pass: process.env.JENKINS_API_KEY
+                    }
+                }, function (err, resp, body) {
+                    var jsonBody = JSON.parse(body);
+                    var parameters = jsonBody.actions[0].parameters;
+                    var channel = req.body.channel_name === 'directmessage' ?
+                        req.body.channel_id : '#' + req.body.channel_name;
+
+                    payload = {
+                        channel: channel,
+                        text: "Branch: " + parameters.value
+                    };
+
+                    request.post({
+                        url: 'https://hooks.slack.com/services/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',
+                        form: { payload: JSON.stringify(payload) }
+                        }, function (err, resp, body) {
+
+                            if (err) {
+                              return res.status(500).send({ success: false, error: err.message });
+                            }
+
+                            res.end();
+                        });
+                });
+        });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
